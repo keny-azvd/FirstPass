3e55a56155af7ab85fd714e92d363fd35e7982c7
keny-azvd <kenner-azevedo@hotmail.com>
2023-11-25 17:02:58 -0300

    add hash method and decrypt method

--- pyqt.py
+++ pyqt.py
@@ -1,12 +1,15 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QVBoxLayout, QPushButton, QTableWidget, QTableWidgetItem
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem, QPushButton,  QHeaderView
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTableWidget, QTableWidgetItem, QPushButton,  QHeaderView 
 from PyQt6.QtCore import Qt
 from cryptography.hazmat.primitives.asymmetric import rsa , padding
 from cryptography.hazmat.primitives import serialization 
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+import os
+import hashlib
 
 
 class PasswordForm(QWidget):
@@ -50,8 +53,8 @@ class PasswordForm(QWidget):
         #generate private encrypt and storage
         private_key = rsa.generate_private_key(public_exponent=65537,key_size=2048)
         pem = private_key.private_bytes(encoding=serialization.Encoding.PEM,format=serialization.PrivateFormat.PKCS8,encryption_algorithm=serialization.BestAvailableEncryption(b'kenner'))
-        f = open(f"./private_keys/{secret}.txt", "w")
-        f.write(pem.decode("utf-8"))
+        f = open(f"./private_keys/{secret}.bin", "wb")
+        f.write(pem)
         f.close()
         public_key = private_key.public_key()
 
@@ -67,6 +70,9 @@ class PasswordForm(QWidget):
         f = open(f"./secrets/{secret}.bin", "wb")
         f.write(ciphertext)
         f.close()
+        self.close()  # close the current window
+        self.reload = MainForm() 
+        self.reload.show() # reload main form
         return None 
 
 
@@ -81,31 +87,15 @@ class MainForm(QWidget):
     def init_ui(self):
         # Widgets para a nova tela
         self.label_welcome = QLabel('Password Manager!')
-        self.query_button = QPushButton('Show Password')
         self.query_button_addpwd = QPushButton('Add new password')
 
         # Layout da nova tela
         layout = QVBoxLayout()
         layout.addWidget(self.label_welcome)
-        layout.addWidget(self.query_button)
         layout.addWidget(self.query_button_addpwd)
         self.table_widget = QTableWidget()
-
-        # Configurar a tabela com 3 linhas e 2 colunas como exemplo
-        self.table_widget.setRowCount(3)
-        self.table_widget.setColumnCount(2)
-        a = '1'
-        # Preencher a tabela com dados de exemplo
-        secrets , hashs = self.read_content()
-
-        for row in range(len(secrets)):
-            for col in range(2):
-                if(col < 1):
-                    item = QTableWidgetItem(f"{secrets[row]}")
-                else:
-                    item = QTableWidgetItem(f"{hashs[row]}")
-                self.table_widget.setItem(row, col, item)
-                item = self.table_widget.item(row, col)
+        
+        self.write_table()
 
         # Definir o layout principal da nova tela
         self.setLayout(layout)
@@ -114,33 +104,75 @@ class MainForm(QWidget):
         self.table_widget.setColumnWidth(0, 170)
 
         # Conectar o botão de consulta e inserção a uma função
-        self.query_button.clicked.connect(self.show_pwd)
         self.query_button_addpwd.clicked.connect(self.add_pwd)
+        self.table_widget.cellClicked.connect(self.cell_clicked)
+        self.table_widget.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
+
 
         # Configuração da nova tela
         self.setWindowTitle('RSA Pass')
         self.setGeometry(500, 500, 400, 400)
 
-    def show_pwd(self):
-        print('Fazer consultas aqui')
-    
+    def write_table(self):
+        secrets , hashs = self.read_content()
+        # Configurar a tabela com 3 linhas e 2 colunas como exemplo
+        self.table_widget.setRowCount(len(secrets))
+        self.table_widget.setColumnCount(2)
+
+        for row in range(len(secrets)):
+            for col in range(2):
+                if(col < 1):
+                    item = QTableWidgetItem(f"{secrets[row]}")
+                else:
+                    item = QTableWidgetItem(" .... ")
+                self.table_widget.setItem(row, col, item)
+                item = self.table_widget.item(row, col)    
+
     def add_pwd(self):
         self.passwordform = PasswordForm()
         self.passwordform.show()
+        self.close()
     
     def read_content(self):
-        with open('base.txt') as f:
-            lines = f.readlines()
-            secret_list = []
-            hash_list = []
-            for i in range(len(lines)-1):
-                secret, hash  = lines[i].split(':')
-                secret_list.append(secret)
-                hash_list.append(hash)
-        f.close()
-        return secret_list , hash_list
+        dir_path = './secrets/'
+        secrets_list = []
+        for path in os.listdir(dir_path):
+            secrets_list.append(path.split('.')[0])
+        return secrets_list , None
+    
+    def cell_clicked(self, row, col):
+        item = self.table_widget.item(row, col)
+        if item:
+            value = item.text()
+            f = open(f"./secrets/{value}.bin", "rb")
+            private_key = open(f"./private_keys/{value}.bin", "rb")
+            
+            pem = private_key.read()
+            data_encrypted = f.read()
 
+            password = b'kenner'
 
+            # Carregue a chave privada usando o PEM e a senha
+            key = serialization.load_pem_private_key(
+                pem,
+                password=password,
+                backend=default_backend()
+            )
+
+            plaintext = key.decrypt(
+                        data_encrypted,
+                        padding.OAEP(
+                            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                            algorithm=hashes.SHA256(),
+                            label=None
+                        )
+                    )
+            print(plaintext.decode('utf-8'))
+
+                                    
+            
+
+            
 
 
 class MyForm(QWidget):
@@ -155,33 +187,45 @@ class MyForm(QWidget):
         self.submit_button = QPushButton('Enviar')
 
         # Layout do formulário de senha
-        layout = QVBoxLayout()
-        layout.addWidget(self.edit_password)
-        layout.addWidget(self.submit_button)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.edit_password)
+        self.layout.addWidget(self.submit_button)
         self.edit_password.setEchoMode(QLineEdit.EchoMode.Password)
 
         # Definir o layout principal da janela
-        self.setLayout(layout)
+        self.setLayout(self.layout)
 
         # Configuração do botão de envio
         self.submit_button.clicked.connect(self.submit_form)
 
+
         # Configuração da janela principal
         self.setWindowTitle('RSA Pass')
-        self.setGeometry(300, 300, 400, 200)
+        self.setGeometry(800, 800, 400, 200)
+        
+        
 
     def submit_form(self):
-        # Exemplo de ação ao clicar no botão de envio
+        # create hash of password input
         password = self.edit_password.text()
-        self.new_form = MainForm()
-        self.new_form.show()
-        self.close()
-        if password == 'kenner':
+        password_hash = self.hash_password(password)
+        # get hash of correct password
+        master_data = open(f"./master.txt", "r")
+        master_hash = master_data.read()
+        if(password_hash == master_hash.strip()):
             print('Você está dentro')
-            # Se a senha estiver correta, instanciar e mostrar a nova tela
+            # if password is correct, will allow go to main form
             self.new_form = MainForm()
             self.new_form.show()
             self.close()
+
+    
+
+    def hash_password(self,password):
+        password_bytes = password.encode('utf-8')
+        hash_object = hashlib.sha256(password_bytes)
+        
+        return hash_object.hexdigest()
 
 
 def main():
